db34fd8c757f14c4a0966bafcd7ae83f7e89cfea
Bushra-B <bushraxbil@gmail.com>
2020-06-30 02:22:12 +0300

    deploy on gh-pages

---
+++ my-app/src/__tests__/components/form/form.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Form from '../../../components/form/form.js';
+
+describe('<Form/>', () => {
+    
+})
